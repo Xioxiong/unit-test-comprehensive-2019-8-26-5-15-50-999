6b3a6b72b76585452422ecb0543e320416e2e967
xiongwei <2894001817@qq.com>
2019-08-26 20:29:32 +0800

    实现了main函数里面的功能

--- src/main/java/tw/Main.java
+++ src/main/java/tw/Main.java
@@ -2,6 +2,9 @@ package tw;
 
 import tw.commands.GuessInputCommand;
 
+import java.lang.reflect.Array;
+import java.util.*;
+
 /**
  * Created by jxzhong on 2017/5/16.
  */
@@ -12,7 +15,30 @@ public class Main {
     public static void main(String[] args) throws Exception {
         for (int i = 0; i < RUN_TIMES; i++) {
             String input = new GuessInputCommand().input();
-            System.out.println(input);
+            List<String> list = Arrays.asList(input.split(""));
+            Map<String,Integer> map = new HashMap<>();
+            Map<String,Integer> map2 = new HashMap<>();
+            Map<String,Integer> map3 = new HashMap<>();
+            for(String str:list){
+                int position = "1234".indexOf(str);
+                if(position != -1){
+                    map.put(str,position);
+                }
+            }
+            map.forEach((t,v)->{
+                if(list.get(v).equals(t)){
+                    map2.put(t,v);
+                }else{
+                    map3.put(t,v);
+                }
+            });
+            if(map2.size() == 4){
+                System.out.println("恭喜你猜对了！");
+                break;
+            }else{
+                System.out.println(map2.size()+"A"+map3.size()+"B");
+            }
         }
+        System.out.println("游戏结束！");
     }
 }
